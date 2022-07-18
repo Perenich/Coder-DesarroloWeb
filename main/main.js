38970c944709610300304a3992b231a8174e2d55
Bruno Perenich <bgperenich@hotmail.com>
2022-07-18 13:38:22 -0300

    Proyecto final desarrolo web

--- main/main.js
+++ main/main.js
@@ -1,4 +1,9 @@
-window.addEventListener("scroll", function(){
-	var header = document.querySelector("header"); 
-	header.classList.toggle("abajo",window.scrollY>0);
-})
+//*window.addEventListener("scroll", function(){
+//	let header = document.querySelector("header"); 
+//	header.classList.toggle("abajo",window.scrollY>0);
+//})
+
+
+document.getElementById('btnEnviar').onclick = function(){
+    alert('Sus datos fueron cargados correctamente, no estaremos contactando con usted, gracias!');
+}
