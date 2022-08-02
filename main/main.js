fa64a6462094312298a07160fd90c3f44dc2b2f8
Bruno Perenich <bgperenich@hotmail.com>
2022-08-02 19:44:34 -0300

    interactuar HTML

--- main/main.js
+++ main/main.js
@@ -1,9 +1,27 @@
-//*window.addEventListener("scroll", function(){
-//	let header = document.querySelector("header"); 
-//	header.classList.toggle("abajo",window.scrollY>0);
-//})
+window.addEventListener("scroll", function(){
+	let header = document.querySelector("header"); 
+	header.classList.toggle("abajo",window.scrollY>0);
+})
 
 
-document.getElementById('btnEnviar').onclick = function(){
+ document.getElementById('btnEnviar').onclick = function(){
     alert('Sus datos fueron cargados correctamente, no estaremos contactando con usted, gracias!');
 }
+ 
+
+const servicios = ['Esculpida', 'Esmaltada', 'Kipping']
+
+servicios.push('Art Nail')
+
+servicios.unshift('Esmaltado Sintetico')
+
+for (let i=0; i < servicios.length ; i++){
+    console.log('Servicio: '+ servicios[i])
+}
+
+
+console.log(servicios)
+
+
+
+
